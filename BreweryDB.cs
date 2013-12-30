83d879690a331bd948d8dbbf74e2a5871a583546
ericwestern <eric.western@gmail.com>
2013-12-30 15:15:49 -0500

    Initial Commit

---
+++ BreweryDB.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.IO;
+
+namespace Simple_BreweryDB_Wrapper
+{
+    public class BreweryDB
+    {
+        const string baseUrl = "http://api.brewerydb.com/v2/";
+        private string apiKey, endpoint, responseFormat;
+        Dictionary<string, string> parameters = new Dictionary<string,string>();
+
+        public BreweryDB(string key, string format)
+        {
+            this.apiKey = key;
+            if (format == null)
+                responseFormat = "json";
+            else
+                responseFormat = format;
+        }
+
+        public string Endpoint
+        {
+            get { return this.endpoint; }
+            set { this.endpoint = value; }
+        }
+
+        public Dictionary<string, string> Parameters
+        {
+            get { return this.parameters; }
+            set { this.parameters = value; }
+        }
+
+
+        public string ExecuteGetCall(string page)
+        {
+            StringBuilder urlRequestBuilder = new StringBuilder(baseUrl);
+            urlRequestBuilder.Append(this.endpoint);
+            urlRequestBuilder.Append("?");
+            foreach (KeyValuePair<string, string> parameter in this.parameters)
+            {
+                urlRequestBuilder.Append(parameter.Key);
+                urlRequestBuilder.Append("=");
+                urlRequestBuilder.Append(parameter.Value);
+                urlRequestBuilder.Append("&");
+            }
+            if (page != null)
+            {
+                urlRequestBuilder.Append("p=");
+                urlRequestBuilder.Append(page);
+                urlRequestBuilder.Append("&");
+            }
+
+            urlRequestBuilder.Append("format=");
+            urlRequestBuilder.Append(responseFormat);
+            urlRequestBuilder.Append("&key=");
+            urlRequestBuilder.Append(apiKey);
+            string urlRequest = urlRequestBuilder.ToString();
+
+            WebRequest request = WebRequest.Create(urlRequest);
+            WebResponse response = request.GetResponse();
+
+            StreamReader reader = new StreamReader(response.GetResponseStream());
+            string results = reader.ReadToEnd();
+            return results;
+        }
+    }
+}
